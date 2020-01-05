0d89080004eeedfe7ec81b0581191c09b8b52816
Richard Kirby <richard.james.kirby@gmail.com>
2020-01-05 22:12:24 +0000

    Now includes semaphore flagger, which has a left and right servo.  Also accepts negative angles to invert the logic for calculating setvo command.

--- main.py
+++ main.py
@@ -17,17 +17,37 @@ out, err = p.communicate()
 
 if len(out.strip()) == 0:
     os.system("sudo pigpiod")
+    time.sleep(3)
 
 pi = pigpio.pi()
 
+class Servo:
 
-class Servo(threading.Thread):
-    def __init__(self, pwm_pin, low_duty, high_duty, pause_time):
-        threading.Thread.__init__(self)
-
+    def __init__(self, pwm_pin, low_duty, high_duty):
         self.pwm_pin = pwm_pin
         self.low_duty = low_duty
         self.high_duty = high_duty
+
+    def set_angle(self, angle):
+
+        if angle < 0:
+            pass
+            servo_pulse = self.high_duty + (float(angle / 180) * (self.high_duty - self.low_duty))
+
+        else:
+            servo_pulse = int((float(angle / 180) * (self.high_duty - self.low_duty)) + self.low_duty)
+
+        print(angle, servo_pulse)
+        pi.set_servo_pulsewidth(self.pwm_pin, int(servo_pulse))
+
+
+class SemaphoreFlagger(threading.Thread):
+
+    def __init__(self, left_servo, right_servo, pause_time):
+        threading.Thread.__init__(self)
+
+        self.left_servo = left_servo
+        self.right_servo = right_servo
         self.pause_time = pause_time
         self.cmd_queue = queue.Queue()
 
@@ -38,11 +58,12 @@ class Servo(threading.Thread):
             while True:
 
                 if not self.cmd_queue.empty():
-                    angle = self.cmd_queue.get_nowait()
-                    servo_pulse = int((float(angle/180) * (self.high_duty - self.low_duty))+ self.low_duty)
+                    left_angle, right_angle = self.cmd_queue.get_nowait()
 
-                    print(angle, servo_pulse)
-                    pi.set_servo_pulsewidth(self.pwm_pin, int(servo_pulse))
+                    self.left_servo.set_angle(left_angle)
+                    self.right_servo.set_angle(right_angle)
+
+                    print(left_angle, right_angle)
 
                 time.sleep(self.pause_time)
 
@@ -55,44 +76,33 @@ class Servo(threading.Thread):
 
 if __name__ == "__main__":
 
-    left_servo = Servo(27, 500, 2500, 0.5)
-    right_servo = Servo(9, 500, 2500, 0.5)
+    left_servo = Servo(27, 500, 2500)
+    right_servo = Servo(9, 500, 2500)
 
-    left_servo.start()
-    right_servo.start()
+    semaphore_flagger = SemaphoreFlagger(left_servo, right_servo, 0.6)
+
+    semaphore_flagger.start()
+
 
     print("home")
-    left_servo.cmd_queue.put_nowait(0)
-    right_servo.cmd_queue.put_nowait(0)
+    semaphore_flagger.cmd_queue.put_nowait((0,0))
     time.sleep(5)
 
 
     try:
         while True:
-            left_servo.cmd_queue.put_nowait(0)
-            left_servo.cmd_queue.put_nowait(30)
-            left_servo.cmd_queue.put_nowait(60)
-            left_servo.cmd_queue.put_nowait(90)
-            left_servo.cmd_queue.put_nowait(120)
-            left_servo.cmd_queue.put_nowait(150)
-            left_servo.cmd_queue.put_nowait(180)
 
-            right_servo.cmd_queue.put_nowait(0)
-            right_servo.cmd_queue.put_nowait(30)
-            right_servo.cmd_queue.put_nowait(60)
-            right_servo.cmd_queue.put_nowait(90)
-            right_servo.cmd_queue.put_nowait(120)
-            right_servo.cmd_queue.put_nowait(150)
-            right_servo.cmd_queue.put_nowait(180)
+            semaphore_flagger.cmd_queue.put_nowait((-1, 0))
+            semaphore_flagger.cmd_queue.put_nowait((-30, 30))
+            semaphore_flagger.cmd_queue.put_nowait((-90, 90))
+            semaphore_flagger.cmd_queue.put_nowait((-120, 120))
+            semaphore_flagger.cmd_queue.put_nowait((-180, 180))
 
             print("start")
             time.sleep(1)
 
 
     except KeyboardInterrupt:
-        right_servo.cmd_queue.put_nowait(0)
-        left_servo.cmd_queue.put_nowait(0)
-        time.sleep(1)
         print("Quitting the program due to Ctrl-C")
 
     except:
